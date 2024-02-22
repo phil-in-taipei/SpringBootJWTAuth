7a098dc5506624f2de234cfeaff47c5863008781
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-02-22 17:15:29 +0800

    Test commandline runner setup for creation of test users for endpoint testing

---
+++ src/main/java/JWTDockerTutorial/security/SecurityApplicationTest.java
@@ -0,0 +1,55 @@
+package JWTDockerTutorial.security;
+
+import JWTDockerTutorial.security.models.user.Role;
+import JWTDockerTutorial.security.models.user.User;
+import JWTDockerTutorial.security.repositories.user.UserRepository;
+import JWTDockerTutorial.security.services.user.UserDetailsServiceImplementation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+@Profile("test")
+public class SecurityApplicationTest implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //@Autowired
+    //private UserDetailsServiceImplementation userService;
+
+    public static void main(String[] args) {
+        SpringApplication
+                .run(SecurityApplication.class, args);
+    }
+
+
+    @Override
+    //@Transactional
+    public void run(String... args) throws Exception {
+        System.out.println("*****************Running Tests**********************");
+        if (userRepository.findAll().isEmpty()) {
+            System.out.println("The user repo is empty");
+            User testUser = User.builder()
+                    .givenName("Test")
+                    .surname("User")
+                    .username("TestUser")
+                    .email("test@gmx.com")
+                    .password("testpassword")
+                    .role(Role.USER)
+                    .build();
+            userRepository.save(testUser);
+            User testAdmin = User.builder()
+                    .givenName("Test")
+                    .surname("Admin")
+                    .username("TestAdmin")
+                    .email("test@gmx.com")
+                    .password("testpassword")
+                    .role(Role.ADMIN)
+                    .build();
+            userRepository.save(testAdmin);
+        }
+    }
+}
--- src/test/java/JWTDockerTutorial/security/SecurityApplicationTests.java
+++ src/test/java/JWTDockerTutorial/security/SecurityApplicationTests.java
@@ -6,17 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
-@Profile("test")
-class SecurityApplicationTests implements CommandLineRunner {
+class SecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Override
-	//@Transactional
-	public void run(String... args) throws Exception {
-		System.out.print("*****************Running Tests**********************");
 	}
 
 }
